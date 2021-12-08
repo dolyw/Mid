92b90ba6dddaf8f421ef9ccb84109226381afc78
dolyw <i@dolyw.com>
2021-12-08 09:34:30 +0800

    knife4j配置调整

--- auth-app/src/main/java/com/pcic/config/Knife4jConfiguration.java
+++ auth-app/src/main/java/com/pcic/config/Knife4jConfiguration.java
@@ -1,18 +1,18 @@
 package com.pcic.config;
 
 import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
- * ADAPT
+ * Knife4jConfiguration
  *
  * @author wliduo[i@dolyw.com]
  * @date 2021/11/23 9:27
@@ -21,6 +21,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableKnife4j
 @EnableSwagger2
 public class Knife4jConfiguration {
+
+    @Value("${spring.application.name}")
+    private String applicationName;
 
     /**
      * knife4jApi
@@ -35,13 +38,13 @@ public class Knife4jConfiguration {
     public Docket knife4jApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(new ApiInfoBuilder()
-                        .title("Mid平台系统Api接口")
-                        .description("Mid平台系统Api对外接口")
+                        .title(applicationName + "系统Api接口")
+                        .description(applicationName + "系统Api对外接口")
                         // .termsOfServiceUrl("http://www.xx.com/")
                         // .contact(new Contact("作者", "地址", "邮箱或联系方式"))
                         .version("1.0")
                         .build())
-                .groupName("Api接口")
+                .groupName(applicationName + "系统Api接口")
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.pcic.api"))
                 .paths(PathSelectors.any())
@@ -61,13 +64,13 @@ public class Knife4jConfiguration {
     public Docket knife4jWebApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(new ApiInfoBuilder()
-                        .title("Mid平台系统Web接口")
-                        .description("Mid平台系统Web前端接口")
+                        .title(applicationName + "系统Web接口")
+                        .description(applicationName + "系统Web前端接口")
                         // .termsOfServiceUrl("http://www.xx.com/")
                         // .contact(new Contact("作者", "地址", "邮箱或联系方式"))
                         .version("1.0")
                         .build())
-                .groupName("Web接口")
+                .groupName(applicationName + "系统Web接口")
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.pcic.web"))
                 .paths(PathSelectors.any())
--- auth-web/src/main/java/com/pcic/web/IndexController.java
+++ auth-web/src/main/java/com/pcic/web/IndexController.java
@@ -2,6 +2,7 @@ package com.pcic.web;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,10 +20,13 @@ import springfox.documentation.annotations.ApiIgnore;
 @RequestMapping("/")
 public class IndexController {
 
+    @Value("${spring.application.name}")
+    private String applicationName;
+
     @ApiOperation(value = "应用首页-展示", notes = "应用首页-展示")
     @GetMapping("/")
     public String index() {
-        return "Hello World Auth";
+        return "Hello World " + applicationName;
     }
 
 }
