3f89edfd3e6ac1caa4ff759971357cd8c5b9f77d
dolyw <i@dolyw.com>
2021-12-08 14:06:27 +0800

    share模块版本配置调整

--- message-app/src/main/java/com/pcic/Application.java
+++ message-app/src/main/java/com/pcic/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Auth鉴权中心
+ * Message消息中心
  *
  * @author wliduo[i@dolyw.com]
  * @date 2021/10/27 11:32
